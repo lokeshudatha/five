affe67bbaea98726743a603d72f9855873728925
lokesh udatha <138110408+lokeshudatha@users.noreply.github.com>
2025-12-10 12:56:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -44,6 +44,7 @@ resource "google_compute_instance" "loki" {
     sudo chmod 666 /var/run/docker.sock
     sudo systemctl restart docker
     sudo git clone https://github.com/lokeshudatha/four
+    sudo cd four
     docker build -t python_img:latest .
     echo $DOCKERHUB_PSW | docker login -u $DOCKERHUB_USR --password-stdin
     docker tag python_img:latest lokeshudatha/python:v1
