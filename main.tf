08ad58217c97de9d0c71bed93867700687869cdd
lokesh udatha <138110408+lokeshudatha@users.noreply.github.com>
2025-12-10 14:20:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "google" {
-  project     = "winter-monolith-477705-m8"
-  region      = "us-central1"
+  project = "winter-monolith-477705-m8"
+  region  = "us-central1"
 }
 
 resource "google_compute_network" "lokinetwork" {
   name                    = "lokesh-network"
-  auto_create_subnetworks = false 
+  auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "lokisubnetwork" {
@@ -31,41 +31,49 @@ resource "google_compute_instance" "loki" {
     access_config {}
   }
 
-  depends_on = [
-    google_compute_subnetwork.lokisubnetwork
-  ]
-    metadata = {
-    ssh-keys = "udathalokesh11:${file("~/.ssh/id_rsa.pub")}"
+  metadata = {
+    ssh-keys = "udathalokesh11:${file("/home/lokesh/.ssh/id_rsa.pub")}"
   }
 
+  # Upload project files (Dockerfile etc.)
+  provisioner "file" {
+    source      = "./app"  # Your local folder that contains Dockerfile
+    destination = "/home/udathalokesh11/app"
+
+    connection {
+      type        = "ssh"
+      user        = "udathalokesh11"
+      private_key = file("/home/lokesh/.ssh/id_rsa")
+      host        = self.network_interface[0].access_config[0].nat_ip
+    }
+  }
+
+  # Install docker + build + push image
   provisioner "remote-exec" {
     inline = [
-      "sudo apt-get update",
+      "sudo apt-get update -y",
       "sudo apt-get install -y docker.io",
-     "sudo systemctl start docker",
-     "sudo systemctl enable docker",
-    "sudo usermod -aG docker udathalokesh11",
-    "sudo chmod 666 /var/run/docker.sock",
-    "sudo systemctl restart docker",
-    "docker build -t python_img:latest .",
-    "echo $DOCKERHUB_PSW | docker login -u $DOCKERHUB_USR --password-stdin",
-    "docker tag python_img:latest lokeshudatha/python:v1",
-    "docker push lokeshudatha/python:v1"
+      "sudo systemctl start docker",
+      "sudo usermod -aG docker udathalokesh11",
+      "cd /home/udathalokesh11/app",
+      "docker build -t python_img:latest .",
+      "echo $DOCKERHUB_PSW | docker login -u $DOCKERHUB_USR --password-stdin",
+      "docker tag python_img:latest lokeshudatha/python:v1",
+      "docker push lokeshudatha/python:v1"
     ]
 
     connection {
       type        = "ssh"
       user        = "udathalokesh11"
-      private_key = file("/home/udathalokesh11/.ssh/id_rsa")
+      private_key = file("/home/lokesh/.ssh/id_rsa")
       host        = self.network_interface[0].access_config[0].nat_ip
     }
   }
 }
+
 resource "google_compute_firewall" "lokifirewall" {
   name    = "lokesh-firewall"
   network = google_compute_network.lokinetwork.id
-
-  direction = "INGRESS"
 
   allow {
     protocol = "all"
@@ -73,5 +81,3 @@ resource "google_compute_firewall" "lokifirewall" {
 
   source_ranges = ["0.0.0.0/0"]
 }
-
-
